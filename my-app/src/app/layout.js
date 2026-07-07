a7b4633a3b3264f5a911098d3a4d1eb3bd62fd02
Bilal sheikh <bilalsheikhsb77@gmail.com>
2026-07-07 15:39:55 +0500

    done

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -5,17 +5,18 @@ import "./globals.css";
 
 export const metadata = {
   title: "BilalProtfolio",
-  verification:{
-    google:"xROAJVgKDmEhC14XIs"
+  verification: {
+    google: "xROAJVgKDmEhC14XIsNC3pRCy42UG3ooyXboA2skxg4"
   },
   description: "Bilal is an AI-driven web developer crafting custom portfolio sites, smart web apps, and automation-first digital products with polished design and modern UX.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={` h-full antialiased`} 
-    suppressHydrationWarning 
+    <html lang="en" className={` h-full antialiased`}
+      suppressHydrationWarning
     >
+
       <body className="min-h-full flex flex-col font-sekuya">
         {children}
       </body>
