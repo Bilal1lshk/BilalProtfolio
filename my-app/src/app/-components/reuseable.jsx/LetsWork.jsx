80f6839ff7cfdba80087e1a4cde329c337c5c26b
Bilal sheikh <bilalsheikhsb77@gmail.com>
2026-07-10 12:18:57 +0500

    done

--- my-app/src/app/-components/reuseable.jsx/LetsWork.jsx
+++ my-app/src/app/-components/reuseable.jsx/LetsWork.jsx
@@ -1,25 +1,53 @@
 import { motion } from 'motion/react'
-import React from 'react'
-export default function LetsWork() {
+import React, { useEffect, useState } from 'react'
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
 
+export default function LetsWork() {
+    const [ismobile, setidmobile] = useState(false)
+    useEffect(() => {
+        const checkscreen = () => {
+            const sheckscreen = setidmobile(window.innerWidth > 640)
+        }
+        checkscreen();
+
+        window.addEventListener("resize", checkscreen)
+
+        return (() => window.removeEventListener("resize", checkscreen))
+
+    }, [])
+    const contaainer = {
+        hidden: { height: "100px" },
+        visible: {
+            height: ismobile?"260px":"160px",
+            transition: {
+                duration: 0.5, ease: "easeInOut"
+            }
+        },
+    }
     return (
-        <div className='bg-g-deep text-white flex w-full h-[800px]  rounded-xl  mb-16'>
+        <div className=' text-black flex w-full h-[900px]  rounded-xl  mb-16'>
             <div className='w-full'>
-                <div>
-                    <div>Instragram</div>
-                    <div>Linkden</div>
-                    <div>Github</div>
+                <div className='flex flex-col md:flex-row flex-wrap justify-between mb-10 p-4'>
+                    <div className='flex flex-col justify-center items-center gap-2'><h3>Instragram</h3> <FaInstagram className='text-2xl' /></div>
+                    <div className='flex flex-col justify-center items-center gap-2'><h3>Linkden</h3> <FaLinkedin className='text-2xl' /></div>
+                    <div className='flex flex-col justify-center items-center gap-2'><h3>Github</h3> <FaGithub className='text-2xl' /></div>
                 </div>
-                <div className='h-full min-w-full bg-amber-50'>
+                <div className='h-full min-w-full '>
                     <div className='flex justify-start w-full'>
-                        <motion.img initial={{ height: "100px" }} whileInView={{ height: "360px" }} transition={{ duration: 0.5, ease: "easeInOut" }}
-                            viewport={{ once: false, amount: 0.5}}
+                        <motion.img variants={contaainer}
+                            initial="hidden"
+                            whileInView="visible"
                             className=' object-cover' src={"/handtwo.png.webp"} alt="" />
 
                     </div>
+                    <div><motion.h5 initial={{ y: 90, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} className=' text-center text-3xl mt-3 '>LETS WORK TOGRTHER</motion.h5></div>
                     <div className='flex justify-end w-full'>
-                        <motion.img initial={{ height: "100px"  }} transition={{ duration: 0.5, ease: "easeInOut" }}
-                            whileInView={{ height: "360px" }} viewport={{ once: false, amount: 0.5 }} className=' object-cover' src={"/handone.png.webp"} alt="" />
+                        <motion.img variants={contaainer}
+                            initial="hidden"
+                            whileInView="visible"
+                            className=' object-cover' src={"/handone.png.webp"} alt="" />
 
                     </div>
                 </div>
